d8be2cead208dda7d2aa8921d94e9d57325d0afc
Sara Shamass <sara.shamass@outlook.com>
2023-02-01 21:14:41 +0200

    Init App

---
+++ frontend/src/app/services/food.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { sample_foods, sample_tags } from 'src/data';
+import { Food } from '../shared/models/Food';
+import { Tag } from '../shared/models/Tags';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FoodService {
+
+  constructor() { }
+
+  getAll() : Food[] {
+    return sample_foods;
+  }
+
+  getAllFoodBySearchTerm(searchTerm:string) : Food[]{
+    return this.getAll().filter(food=> food.name.toLowerCase().includes(searchTerm.toLowerCase()));
+  }
+
+  getAllTags() : Tag[] {
+    return sample_tags;
+  }
+
+  getAllFoodsByTag(tag:string) : Food[] {
+    return tag === 'All'?
+    this.getAll() :
+    this.getAll().filter(food=> food.tags?.includes(tag));
+  }
+
+  getFoodById(id:string) : Food {
+    return this.getAll().find(food=> food.id == id) ?? new Food();
+  }
+}
