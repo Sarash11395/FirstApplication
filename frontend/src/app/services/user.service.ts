73003c85a7f54ac64b41ffb6cfff1b67a8843324
Sara Shamass <sara.shamass@outlook.com>
2023-02-12 19:43:34 +0200

    login and logout

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -6,12 +6,14 @@ import { USER_LOGIN_URL } from '../shared/constants/urls';
 import { IUserLogin } from '../shared/interfaces/IUserLogin';
 import { User } from '../shared/models/User';
 
+const USER_KEY = 'User';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  private userSubject= new BehaviorSubject<User>(new User());
+  private userSubject= new BehaviorSubject<User>(this.gettUserToLocalStorage());
   public userObservable!: Observable<User>;
 
   constructor(private http:HttpClient, private toastrService: ToastrService) {
@@ -23,6 +25,7 @@ export class UserService {
     return this.http.post<User>(USER_LOGIN_URL, userLogin).pipe(
       tap({
         next: (user) =>{
+          this.setUserToLocalStorage(user);
           this.userSubject.next(user);
           this.toastrService.success(
             `Welcome to Foodmine ${user.name}!`,
@@ -34,5 +37,23 @@ export class UserService {
         }
       })
     );
+  }
+
+  logout(){
+    this.userSubject.next(new User());
+    localStorage.removeItem(USER_KEY);
+    window.location.reload;
+  }
+
+  private setUserToLocalStorage(user:User){
+    localStorage.setItem(USER_KEY,JSON.stringify(user));
+  }
+
+  private gettUserToLocalStorage():User{
+    const userJson = localStorage.getItem(USER_KEY);
+    if(userJson){
+      return JSON.parse(userJson) as User;
+    }
+    return new User();
   }
 }
